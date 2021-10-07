c53507a0fb386829ff1b36f78a17f096adadd578
Nicholas King <v-nicki@microsoft.com>
2021-10-06 17:17:18 -0700

    Initial commit

---
+++ geometry.py
@@ -0,0 +1,14 @@
+import math
+
+def calculate_circumference(r: float) -> float:
+    return 2 * math.pi * r
+
+def circle_area(radius: float) -> float:
+    return math.pi * (radius**2)
+
+def rectangle_area(a, b):
+    return a * b
+
+def square_perimeter(side_length):
+    return side_length * 4
+
