72751b668b7e282e2a109d77c35c81b465ee74ba
Nicholas King <v-nicki@microsoft.com>
2021-10-27 18:01:19 -0700

    Add safe tar extract example and test

--- test_unsafe.py
+++ test_unsafe.py
@@ -10,12 +10,12 @@ def test_match_url():
   # Demonstrate vulnerability - this shouldn't work
   assert(match_url('www3example.com/'))
 
-def test_extract_from_zip():
-  def make_tarfile(output_filename, source_dir):
-    # From https://stackoverflow.com/a/17081026
-    with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(source_dir, arcname=os.path.basename(source_dir))
+def make_tarfile(output_filename, source_dir):
+  # From https://stackoverflow.com/a/17081026
+  with tarfile.open(output_filename, "w:gz") as tar:
+      tar.add(source_dir, arcname=os.path.basename(source_dir))
 
+def test_extract_from_zip():
   tmp_dir = os.path.join(os.getcwd(), 'tmp')
   example_dir = os.path.join(tmp_dir, 'example')
   sub_dir = os.path.join(example_dir, '1')
@@ -24,6 +24,24 @@ def test_extract_from_zip():
   os.makedirs(sub_dir)
   make_tarfile(tar_filename, example_dir)
   extract_from_zip(tar_filename, unzip_dir)
+  unzip_example_dir = os.path.join(unzip_dir, 'example')
+  unzip_sub_dir = os.path.join(unzip_example_dir, '1')
+  unzip_example_dir_exists = os.path.exists(unzip_example_dir)
+  unzip_sub_dir_exists = os.path.exists(unzip_sub_dir)
+  shutil.rmtree(tmp_dir)
+  assert(unzip_example_dir_exists)
+  assert(unzip_sub_dir_exists)
+
+def test_extract_from_zip_safely():
+  # This only runs a basic test. It doesn't check for TarSlip.
+  tmp_dir = os.path.join(os.getcwd(), 'tmp')
+  example_dir = os.path.join(tmp_dir, 'example')
+  sub_dir = os.path.join(example_dir, '1')
+  tar_filename = os.path.join(tmp_dir, 'tmp.tar.gz')
+  unzip_dir = os.path.join(tmp_dir, 'tmp_unzip')
+  os.makedirs(sub_dir)
+  make_tarfile(tar_filename, example_dir)
+  extract_from_zip_safely(tar_filename, unzip_dir)
   unzip_example_dir = os.path.join(unzip_dir, 'example')
   unzip_sub_dir = os.path.join(unzip_example_dir, '1')
   unzip_example_dir_exists = os.path.exists(unzip_example_dir)
--- unsafe.py
+++ unsafe.py
@@ -6,7 +6,6 @@ def match_url(url: str, regex=re.compile('(www|beta).example.com/')):
   """ Returns true if the url matches the regex """
   return regex.match(url) is not None
 
-
 def extract_from_zip(zip_name, dest_path):
   """ Extracts a tar or zip file into a folder """
   # example from https://github.com/github/codeql/blob/main/python/ql/src/Security/CWE-022/examples/tarslip_bad.py
@@ -14,3 +13,17 @@ def extract_from_zip(zip_name, dest_path):
       # BAD - relative paths in tar entry can access any file on the filesystem
       for entry in tar:
           tar.extract(entry, dest_path)
+
+def extract_from_zip_safely(zip_name, dest_path):
+  """
+  Safely extracts a tar or zip file into a folder. 
+  Avoids the TarSlip vulnerability by validating paths inside the zip file.
+  """
+  import os
+  # example from https://github.com/github/codeql/blob/main/python/ql/src/Security/CWE-022/examples/tarslip_good.py
+  with tarfile.open(zip_name) as tar:
+    for entry in tar:
+        #GOOD: Check that entry is safe
+        if os.path.isabs(entry.name) or ".." in entry.name:
+            raise ValueError("Illegal tar archive entry")
+        tar.extract(entry, dest_path)
